53b81799bb9c9ae1997c8afbef9c49581e5182b0
hayata-yamamoto <hayata.yamamoto.work@gmail.com>
2024-07-31 17:25:00 +0900

    add new generated files

---
+++ lib/services/settings_service.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'settings_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$settingsServiceHash() => r'ac81092b16d32a8a0e45ea3bf5c65dadbe3a6c84';
+
+/// See also [SettingsService].
+@ProviderFor(SettingsService)
+final settingsServiceProvider =
+    AutoDisposeAsyncNotifierProvider<SettingsService, void>.internal(
+  SettingsService.new,
+  name: r'settingsServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$settingsServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SettingsService = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
