4ea6fac47ba85bbf7566d8f1682877b1fdc6f867
hayata-yamamoto <hayata.yamamoto.work@gmail.com>
2024-08-16 13:02:59 +0900

    fix: presentationの引数をdatabaseのみに修正

--- lib/presentations/home.dart
+++ lib/presentations/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:nose_touch/infra/database.dart';
 import 'package:nose_touch/presentations/settings/main.dart';
-import 'package:nose_touch/services/settings_service.dart';
 
 import 'information_card/main.dart';
 import 'needs_checklist/main.dart';
@@ -12,25 +11,21 @@ class MyHomePage extends HookWidget {
 
   const MyHomePage({super.key, required this.database});
 
-  static const _title = ['情報カード', 'チェックリスト', '設定'];
-
   @override
   Widget build(BuildContext context) {
     final selectedIndex = useState(0);
-    final settingsService = SettingsService(database);
+    const title = ['情報カード', 'チェックリスト', '設定'];
 
     return DefaultTabController(
-        length: _title.length,
+        length: title.length,
         child: Scaffold(
           appBar: AppBar(
-            title: Text(_title.elementAt(selectedIndex.value)),
+            title: Text(title.elementAt(selectedIndex.value)),
           ),
           body: [
             InformationCardView(database: database),
             NeedsChecklistView(database: database),
-            SettingsView(
-              settingsService: settingsService,
-            ),
+            SettingsView(database: database),
           ].elementAt(selectedIndex.value),
           bottomNavigationBar: BottomNavigationBar(
             items: const [
--- lib/presentations/settings/main.dart
+++ lib/presentations/settings/main.dart
@@ -1,30 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:nose_touch/infra/database.dart';
 import 'package:nose_touch/services/settings_service.dart';
 
 class SettingsView extends HookWidget {
-  final SettingsService settingsService;
+  final AppDatabase database;
 
-  const SettingsView({super.key, required this.settingsService});
-
-  void handleDelete(BuildContext context) {
-    settingsService.reset().then((value) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(const SnackBar(content: Text('初期状態に戻しました')));
-      Navigator.of(context).pop();
-    });
-  }
+  const SettingsView({super.key, required this.database});
 
   @override
   Widget build(BuildContext context) {
     final appVersion = useState<String>('');
+    final service = SettingsService(database);
 
     useEffect(() {
-      settingsService.getAppVersion().then((value) {
+      service.getAppVersion().then((value) {
         appVersion.value = value;
       });
       return null;
     }, []);
+
+    void handleDelete() {
+      service.reset().then((value) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text('初期状態に戻しました')));
+        Navigator.of(context).pop();
+      });
+    }
+
+    void handleShowResetDialog() {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(title: const Text('初期状態に戻しますか？'), actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('キャンセル'),
+              ),
+              TextButton(onPressed: handleDelete, child: const Text('はい'))
+            ]);
+          });
+    }
 
     return Scaffold(
         body: ListView(children: [
@@ -41,29 +59,7 @@ class SettingsView extends HookWidget {
         },
       ),
       const Divider(),
-      ListTile(
-          title: const Text('初期状態に戻す'),
-          onTap: () {
-            showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                      title: const Text('初期状態に戻しますか？'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('キャンセル'),
-                        ),
-                        TextButton(
-                            onPressed: () {
-                              handleDelete(context);
-                            },
-                            child: const Text('はい'))
-                      ]);
-                });
-          }),
+      ListTile(title: const Text('初期状態に戻す'), onTap: handleShowResetDialog),
     ]));
   }
 }
